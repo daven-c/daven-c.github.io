b967b6915eb241317a9535d9824abea28b4a7706
xxboomstick <ddchang1iz@gmail.com>
2022-12-10 20:58:55 -0500

    alksjdf

---
+++ scripts/page-switching.js
@@ -0,0 +1,10 @@
+var btn_home = document.querySelector('#home-button');
+var btn_two = document.querySelector('#two-button');
+var btn_three = document.querySelector('#three-button');
+var btn_four = document.querySelector('#four-button');
+var btn_five = document.querySelector('#five-button');
+var text_field = document.querySelector('p');
+
+btn_home.addEventListener('click', () => {
+    text_field.innerText = 'New Text';
+});
