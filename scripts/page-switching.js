e6a739a98b96358418ae5ff73db6057938e5705b
xxboomstick <ddchang1iz@gmail.com>
2022-12-11 22:32:00 -0500

    added functionality

--- scripts/page-switching.js
+++ scripts/page-switching.js
@@ -1,11 +1,33 @@
-var btn_home = document.querySelector('#home-button');
-var btn_two = document.querySelector('#two-button');
-var btn_three = document.querySelector('#three-button');
-var btn_four = document.querySelector('#four-button');
-var btn_five = document.querySelector('#five-button');
-var text_field = document.querySelector('p');
+function get_focused_button() {
+  return document.getElementsByClassName("focused-button")[0];
+}
 
-btn_home.addEventListener('click', () => {
-    text_field.innerText = 'New Text';
-});
- 
+function get_focused_banner() {
+  return document.getElementsByClassName("focused-banner")[0];
+}
+
+function page_swap(target_button) {
+  change_button(target_button);
+  change_banner(target_button.id);
+}
+
+function change_button(new_button) {
+  var current_button = get_focused_button();
+  current_button.classList.remove("focused-button");
+  new_button.classList.add("focused-button");
+}
+
+function change_banner(button_id) {
+  var current_banner = get_focused_banner();
+  var new_banner = document.getElementById(button_to_banner[button_id]);
+  current_banner.classList.remove("focused-banner");
+  new_banner.classList.add("focused-banner");
+}
+
+var button_to_banner = {
+  "home-button": "home-page",
+  "two-button": "two-page",
+  "three-button": "three-page",
+  "four-button": "four-page",
+  "five-button": "five-page",
+};
