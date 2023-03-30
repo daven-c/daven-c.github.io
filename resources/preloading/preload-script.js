9e3d18b36cded05a84665c29df63ff2deea817ed
xxboomstick <ddchang1iz@gmail.com>
2023-03-30 18:08:13 -0400

    Update preload-script.js

--- resources/preloading/preload-script.js
+++ resources/preloading/preload-script.js
@@ -1,13 +1,12 @@
 const preloader = document.getElementById("preload-container");
 
 // handle refresh
-window.scrollTo({ top: 0, behavior: "smooth" });
-window.scrollTo({ top: 0, behavior: "smooth" });
+window.onbeforeunload = preloader.scrollIntoView({ behavior: "smooth" });
 
 preloader.onanimationend = (animation) => {
-  if (animation.animationName == 'vanish') {
+  if (animation.animationName == "vanish") {
     console.log("preload animation ended");
-    document.body.style.overflowY = 'auto';
-    document.body.removeChild(preloader)
+    document.body.style.overflowY = "auto";
+    document.body.removeChild(preloader);
   }
 };
