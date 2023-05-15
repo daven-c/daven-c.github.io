40f2293df6914044d527b7385edabe376b768f8d
Daven <dchang1iz@gmail.com>
2023-05-15 17:49:09 -0400

    typewriter timings

--- resources/scripts.js
+++ resources/scripts.js
@@ -92,6 +92,6 @@ function typeWriter(ele, txt, speed, callback = null) {
 
 setTimeout(() => {
   typeWriter(title, "Daven Chang", 100, () => {
-    typeWriter(subtitle, "Software | Automation | Machine Learning", 100);
+    typeWriter(subtitle, "Software | Automation | Machine Learning", 50);
   });
 }, 1000);
